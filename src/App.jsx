973993a74592f04f376dbd80fcc0a460212ae77a
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2024-12-07 19:00:52 +0530

    Added absolute import configurations using vite.config and jsconfig

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import "./App.css";
 
+import Home from "Pages/Home";
+
 function App() {
-  return <>Home</>;
+  return (
+    <>
+      <Home />
+    </>
+  );
 }
 
 export default App;
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,10 @@
+import Footer from "Components/Footer/Footer";
+
+export default function Home() {
+    return (
+        <>
+        Home Component 
+        <Footer/>
+        </>
+    );
+}
