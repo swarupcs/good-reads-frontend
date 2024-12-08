99a66eec500b7a1ed4ef8e40b414ffe478262a4e
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2024-12-08 11:55:55 +0530

    Completed authentication flow

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,18 @@
 import "./index.css";
 
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom/client";
+import { Toaster } from "react-hot-toast";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import store from "Redux/store";
 
 import App from "./App.jsx";
 
-createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <BrowserRouter>
       <App />
+      <Toaster />
     </BrowserRouter>
   </Provider>
 );
