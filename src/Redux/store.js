9f69a2818811f076322f4943b8a3b3e86061cbe1
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2024-12-13 20:44:20 +0530

    Added book description page along with books cards

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authSliceReducer from "Redux/Slices/AuthSlice";
+import bookSliceReducer from "Redux/Slices/BookSlice";
 export default configureStore({
   reducer: {
     auth: authSliceReducer,
+    book: bookSliceReducer,
   },
   devTools: true,
   middleware: (getDefaultMiddleware) =>
